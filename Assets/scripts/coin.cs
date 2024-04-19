362fb7415f78cb8f3b943243628e242b42eef3f3
zbaghezza1 <zaynah.baghezza@g.austincc.edu>
2024-04-19 10:22:58 -0500

    Initial commit

---
+++ Assets/scripts/coin.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour
+{
+
+    public float rotateSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(0,rotateSpeed,0);
+    }
+}
